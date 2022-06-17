c959b6eb0a021637240f6d9555c313e262d8cd1f
artpaul <artmkin@gmail.com>
2022-06-17 20:40:06 +0500

    added CPack packaging, pkg-config and CMake version files.

--- cmake/cxxopts.cmake
+++ cmake/cxxopts.cmake
@@ -52,9 +52,9 @@ function(cxxopts_use_unicode)
     find_package(PkgConfig)
     pkg_check_modules(ICU REQUIRED icu-uc)
 
-    target_link_libraries(cxxopts PUBLIC ${ICU_LDFLAGS})
-    target_compile_options(cxxopts PUBLIC ${ICU_CFLAGS})
-    target_compile_definitions(cxxopts PUBLIC CXXOPTS_USE_UNICODE)
+    target_link_libraries(cxxopts INTERFACE ${ICU_LDFLAGS})
+    target_compile_options(cxxopts INTERFACE ${ICU_CFLAGS})
+    target_compile_definitions(cxxopts INTERFACE CXXOPTS_USE_UNICODE)
 endfunction()
 
 # Request C++11 without gnu extension for the whole project and enable more warnings
@@ -74,8 +74,10 @@ function(cxxopts_enable_warnings)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W2")
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
       if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+          if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL 5.0)
             set(COMPILER_SPECIFIC_FLAGS "-Wsuggest-override")
-        endif()
+          endif()
+      endif()
 
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -Wextra -Wshadow -Weffc++ -Wsign-compare -Wshadow -Wwrite-strings -Wpointer-arith -Winit-self -Wconversion -Wno-sign-conversion ${COMPILER_SPECIFIC_FLAGS}")
     endif()
@@ -85,18 +87,32 @@ endfunction()
 
 # Helper function to ecapsulate install logic
 function(cxxopts_install_logic)
-    set(CXXOPTS_CMAKE_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/cxxopts" CACHE STRING "Installation directory for cmake files, relative to ${CMAKE_INSTALL_PREFIX}.")
+    if(CMAKE_LIBRARY_ARCHITECTURE)
+        string(REPLACE "/${CMAKE_LIBRARY_ARCHITECTURE}" "" CMAKE_INSTALL_LIBDIR_ARCHIND "${CMAKE_INSTALL_LIBDIR}")
+    else()
+        # On some systems (e.g. NixOS), `CMAKE_LIBRARY_ARCHITECTURE` can be empty
+        set(CMAKE_INSTALL_LIBDIR_ARCHIND "${CMAKE_INSTALL_LIBDIR}")
+    endif()
+    set(CXXOPTS_CMAKE_DIR "${CMAKE_INSTALL_LIBDIR_ARCHIND}/cmake/cxxopts" CACHE STRING "Installation directory for cmake files, relative to ${CMAKE_INSTALL_PREFIX}.")
     set(version_config "${PROJECT_BINARY_DIR}/cxxopts-config-version.cmake")
     set(project_config "${PROJECT_BINARY_DIR}/cxxopts-config.cmake")
     set(targets_export_name cxxopts-targets)
+    set(PackagingTemplatesDir "${PROJECT_SOURCE_DIR}/packaging")
+
+
+    if(${CMAKE_VERSION} VERSION_GREATER "3.14")
+        set(OPTIONAL_ARCH_INDEPENDENT "ARCH_INDEPENDENT")
+    endif()
 
     # Generate the version, config and target files into the build directory.
     write_basic_package_version_file(
         ${version_config}
         VERSION ${VERSION}
-        COMPATIBILITY AnyNewerVersion)
+        COMPATIBILITY AnyNewerVersion
+        ${OPTIONAL_ARCH_INDEPENDENT}
+    )
     configure_package_config_file(
-        ${PROJECT_SOURCE_DIR}/cxxopts-config.cmake.in
+        ${PackagingTemplatesDir}/cxxopts-config.cmake.in
         ${project_config}
         INSTALL_DESTINATION ${CXXOPTS_CMAKE_DIR})
     export(TARGETS cxxopts NAMESPACE cxxopts::
@@ -112,4 +128,36 @@ function(cxxopts_install_logic)
     # Install the header file and export the target
     install(TARGETS cxxopts EXPORT ${targets_export_name} DESTINATION ${CMAKE_INSTALL_LIBDIR})
     install(FILES ${PROJECT_SOURCE_DIR}/include/cxxopts.hpp DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+
+
+    set(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
+    set(CPACK_PACKAGE_VENDOR "cxxopt developers")
+    set(CPACK_PACKAGE_DESCRIPTION "${PROJECT_DESCRIPTION}")
+    set(CPACK_DEBIAN_PACKAGE_NAME "${CPACK_PACKAGE_NAME}")
+    set(CPACK_RPM_PACKAGE_NAME "${CPACK_PACKAGE_NAME}")
+    set(CPACK_PACKAGE_HOMEPAGE_URL "${PROJECT_HOMEPAGE_URL}")
+    set(CPACK_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR}")
+    set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_MAINTAINER}")
+    set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+    set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+
+    set(CPACK_DEBIAN_PACKAGE_NAME "lib${PROJECT_NAME}-dev")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6-dev")
+    set(CPACK_DEBIAN_PACKAGE_SUGGESTS "cmake, pkg-config, pkg-conf")
+
+    set(CPACK_RPM_PACKAGE_NAME "lib${PROJECT_NAME}-devel")
+    set(CPACK_RPM_PACKAGE_SUGGESTS "${CPACK_DEBIAN_PACKAGE_SUGGESTS}")
+
+    set(CPACK_DEB_COMPONENT_INSTALL ON)
+    set(CPACK_RPM_COMPONENT_INSTALL ON)
+    set(CPACK_NSIS_COMPONENT_INSTALL ON)
+    set(CPACK_DEBIAN_COMPRESSION_TYPE "xz")
+
+    set(PKG_CONFIG_FILE_NAME "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc")
+    configure_file("${PackagingTemplatesDir}/pkgconfig.pc.in" "${PKG_CONFIG_FILE_NAME}" @ONLY)
+    install(FILES "${PKG_CONFIG_FILE_NAME}"
+            DESTINATION "${CMAKE_INSTALL_LIBDIR_ARCHIND}/pkgconfig"
+    )
+
+    include(CPack)
 endfunction()
